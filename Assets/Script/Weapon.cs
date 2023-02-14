85f0ae379d615c94588ccaf0e577a3e99c972885
SuHwann <dltnghks3256@gmail.com>
2023-02-15 00:01:52 +0900

    weapon.cs 코딩중

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //무기 ID , 프리펩 ID , 데미지 , 개수 , 속도 변수
+    public int id;
+    public int prefabID;
+    public float damage;
+    public int count;
+    public float speed;
+    private void Update()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Init()
     {
-        
+        switch(id)
+        {
+            case 0:
+                speed = -150;
+                Batch();
+                break;
+            default:
+                break;
+        }
+    }
+    void Batch()
+    {
+        for(int index =0; index < count; index++) {
+            Transform bullet = GameManager.Instance.pool.Get(prefabID).transform;
+            bullet.parent = transform; //bullet 부모 속성 변경
+
+        }
     }
 }
